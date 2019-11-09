499f6ed428f9760d5fc292292993361d8aed5026
bussakorn033 <nan15594@gmail.com>
2019-11-09 16:18:42 +0700

    add index

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://29bfc3c55cc646b88d6239ebc0e96b79@sentry.io/1814038' });
 
-const calculator = require('./calculator')
+Sentry.configureScope(function(scope) {
+    scope.setTag("5735512033", "nan15594@gmail.com");
+    scope.setUser({
+      email: "nan15594@gmail.com",
+      username: 'Bussakorn Saetiew’'
+    });
+   });
+// const calculator = require('./calculator')
 myUndefinedFunction();
+
